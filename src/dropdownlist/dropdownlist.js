0c235a6f16900aafe604a5cf187e4560e655201d
Mateen Kadwaikar <mkadwaikar@gmail.com>
2020-04-17 14:27:27 +0530

    CSS changes

--- src/dropdownlist/dropdownlist.js
+++ src/dropdownlist/dropdownlist.js
@@ -2,33 +2,22 @@ import React, { useEffect } from 'react'
 import { getDropDownListService } from '../redux/service';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import FormControl from '@material-ui/core/FormControl';
-import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux'
-
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  mainDiv: {
-    display: "flex",
-    justifyContent: "center",
-  }
-}));
+import "./dropdown.css"
 
 const DropDownList = ({ handleCountryEvent }) => {
   const dispatch = useDispatch();
-  const classes = useStyles();
 
   let dropDownList = useSelector((state) => state.getDropDownListReducer.dropDownList);
+
   useEffect(() => {
     dispatch(getDropDownListService())
   }, [dispatch])
 
   return (
     dropDownList && dropDownList.length ?
-      <div className={classes.mainDiv}>
-        <FormControl className={classes.formControl}>
+      <div className="mainDiv">
+        <FormControl className="formControl">
           <NativeSelect defaultValue="" onChange={(e) => { handleCountryEvent(e.currentTarget.value) }}>
             <option value="">Global</option>
             {dropDownList.map((data, i) =>
